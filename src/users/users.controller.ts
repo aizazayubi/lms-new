e3fd1c3cc1d02611ff7a25af9b0e0125175ebff2
aizazayubi <aizazayubi@gmail.com>
2026-01-17 21:54:04 +0500

    remove unnecessary blank line in UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,7 +10,6 @@ export class UsersController {
   findAll() {
     return this.usersService.findAll();
   }
-
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(Number(id));
