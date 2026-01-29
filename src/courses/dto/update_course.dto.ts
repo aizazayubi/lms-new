11e400e104cdc55d59533f2bc4a5a311108cd4a8
unknown <aizazayubi777@gmail.com>
2026-01-29 16:10:41 +0500

    Validations added

--- src/courses/dto/update_course.dto.ts
+++ src/courses/dto/update_course.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateCourseDto } from './create-course.dto';
+import { CreateCourseDto } from './create_course.dto';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
 export class UpdateCourseDto extends PartialType(CreateCourseDto) {}
